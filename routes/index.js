72ca79bfe87e4d81b0af7906195ed1a164dffa58
GroverTB <grover.jtb@gmail.com>
2017-04-28 11:43:44 -0500

    [UPDATE] example

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var path    = require('path');
+
+// var fs      = require('fs');
+// var xlsx    = require('node-xlsx');
+// var arraya  = xlsx.parse(path.join(__dirname, '..', 'files', 'ejemplo.xlsx'));
+// var arrayb  = xlsx.parse(fs.readFileSync(path.join(__dirname, '..', 'files', 'ejemplo.xlsx')));
+
+
+var xlsxj = require("xlsx-to-json");
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
+  xlsxj({
+    input: path.join(__dirname, '..', 'files', 'ejemplo.xlsx'),
+    output: "output.json"
+  }, function(err, result) {
+    if(err) {
+      console.error(err);
+    }else {
+      console.log(result);
+    }
+  });
+
   res.render('index', { title: 'Express' });
 });
 
